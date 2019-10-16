ea565da8aea0b49e3d871b6442a3621c666d86a9
ehaffey <e-haffey@hotmail.com>
2019-10-16 13:27:52 -0400

    Add train status button and api call
    -Added a get status button to Train.js, the corresponding api call
    to the MBTA api, and a jsx variable to store and display the response.

--- src/components/Trains/Train.js
+++ src/components/Trains/Train.js
@@ -7,6 +7,46 @@ import apiUrl from './../../apiConfig'
 const Train = ({ user, match, alerts }) => {
   const [train, setTrain] = useState([])
   const [deleted, setDeleted] = useState(false)
+  const [trainAlerts, setTrainAlerts] = useState([{
+    attributes: {
+      header: 'None!',
+      timeframe: 'N/A'
+    } }])
+
+  const mbtaGet = () => {
+    axios({
+      method: 'GET',
+      url: `https://api-v3.mbta.com/alerts?filter%5Bactivity%5D=BOARD%2CEXIT%2CRIDE&filter%5Broute%5D=${train.line}`,
+      headers: {
+        'accept': 'application/vnd.api+json'
+      }
+    })
+      // .then(responseData =>
+      //   console.log(responseData.data.data))
+      .then(responseData =>
+        (responseData.data.data[0].attributes.header) ? setTrainAlerts(responseData.data.data) : null
+      // console.log(trainAlerts)
+      )
+      // .then(responseData =>
+      //   console.log(trainAlerts))
+      .catch(error => {
+        console.error(error)
+        alert({
+          heading: 'Failed',
+          message: 'This is terribly embarrasing, but we are having trouble loading your data at the moment, perhaps the server is taking a nap. Please try again later.',
+          variant: 'danger'
+        })
+      })
+  }
+
+  // if (alertsJsx[0].attributes.header !== 'None!') {
+  const alertsJsx = trainAlerts.map(trainAlert => (
+    <div key={trainAlert.attributes.header}>
+      <p>Alert: {trainAlert.attributes.header}</p>
+      <p>When: {trainAlert.attributes.timeframe}</p>
+    </div>
+  ))
+  // }
 
   useEffect(() => {
     axios({
@@ -55,11 +95,13 @@ const Train = ({ user, match, alerts }) => {
     <React.Fragment>
       <h3>The following information is being used to track this train</h3>
       <p>Line: {train.line}</p>
-      <p>Station: {train.station}</p>
+      <p>Your Station: {train.station}</p>
       <Link to={`/trains/${match.params.id}/edit`}>
         <Button variant="warning" type="button">Edit</Button>
       </Link>
       <Button variant="danger" onClick={destroy}>Delete</Button>
+      <Button variant="danger" onClick={mbtaGet}>Check Alerts</Button>
+      {alertsJsx}
     </React.Fragment>
   )
 }
